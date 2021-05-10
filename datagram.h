02a372d21ed5d0b4d693da87574b6ac7682425a9
mcamberlin <merlin.camberlin@student.uclouvain.be>
2021-05-10 20:01:35 +0200

    push

--- datagram.h
+++ datagram.h
@@ -16,11 +16,10 @@
 
 typedef struct 
 {
-    u_int8_t code:4 ;       // query or answer ?
-    u_int8_t type_info:4;   // type of information in the payload: temperature information, door information,...
-    u_int8_t id:4;          // ID of a specific sensors 
-    u_int8_t size:4;       // size du payload in Byte
-    int payload;           // payload de 4 bytes
+    u_int8_t code ;       // query or answer ?
+    u_int8_t type_info;   // type of information in the payload: temperature information, door information,...
+    u_int8_t id;            // ID of a specific sensors 
+    char payload[4];        // payload de 4 bytes
 } datagram;
 
 
--- init.py
+++ init.py
@@ -8,6 +8,6 @@ import os
 def init():
     #print(os.getcwd())
     os.chdir("../tools/serial-io/")
-    os.system("sudo ./tunslip6 -a 172.17.0.2 -p 60001 bbbb::1/64")
+    os.system("sudo ./tunslip6 -a 172.17.0.2 -p 60002 bbbb::1/64")
 
 init()
